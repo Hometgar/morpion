eec0f654e1230fd88542acc905a9a5fbfd27cb6d
Hometgar <m.redon@outlook.fr>
2017-06-21 00:31:05 +0200

    debut implementation view
    mise a jour des vues:
    index.pug
    layout.pug
    
    mise a jour du router index.js

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
+let conf = require('../private/conf.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Morpion',
-    css: [
-        '/stylesheet/general.js',
-	    '/stylesheet/index.js'
-    ]});
+	res.status(200).render('index', {
+		title: 'Morpion',
+		css_links: [
+			'/stylesheet/general.css',
+			'/stylesheet/index.css'
+		]});
 });
 
 module.exports = router;
